ece8b23be7e992a7353bcdec95c44fe3a334dd13
8166315 <justen.crockett@g.austincc.edu>
2019-07-16 18:23:00 -0500

    initial commit - hello world

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 06 - FINAL PROJECT
+Purpose:
+Notes:
+***********************************************************/
+
+#include "main.h"
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 06 - FINAL PROJECT
+Purpose:
+Notes:
+***********************************************************/
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <iostream>
+
+using namespace std;
+
+
+#endif //MAIN_H
