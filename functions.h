f47580c73879630c3606397cfd0d4c7441d418c2
justen <justenc94@gmail.com>
2019-07-16 19:47:57 -0500

    added functions and graph header and cpp

---
+++ functions.h
@@ -0,0 +1,11 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 06 - FINAL PROJECT
+Purpose:
+Notes:
+***********************************************************/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#endif //FUNCTIONS_H
---
+++ graph.h
@@ -0,0 +1,11 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 06 - FINAL PROJECT
+Purpose:
+Notes:
+***********************************************************/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#endif //GRAPH_H
