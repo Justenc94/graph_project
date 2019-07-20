b50191fc90d13f2c59c0d1804dc9288945497e36
justen <justenc94@gmail.com>
2019-07-20 16:00:24 -0500

    created data.h to store edge and node structs

---
+++ data.h
@@ -0,0 +1,19 @@
+/***********************************************************
+Name: Justen Crockett
+Assignment: 06 - FINAL PROJECT
+Purpose:
+Notes:
+***********************************************************/
+
+#ifndef DATA_H
+#define DATA_H
+
+struct Node {
+
+};
+
+struct Edge {
+
+};
+
+#endif //DATA_H
--- graph.h
+++ graph.h
@@ -8,6 +8,27 @@ Notes:
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include "data.h"
 
+class Graph {
+
+public:
+    //TODO: Breadth-First traversal method - outputs to console
+    //TODO: Depth-First traversal method - outputs to console
+    //TODO: *BONUS* Point to Point traversal method - outputs to console
+    //TODO: Method to output adjacency matrix to a new data file
+    //TODO: Graph manipulation: Add a vertex
+    //TODO: Graph manipulation: Remove a vertex
+    //TODO: Graph manipulation: Add an edge
+    //TODO: Graph manipulation: Remove an edge
+    //TODO: Graph information: Get number of vertices
+    //TODO: Graph information: Get number of edges
+    //TODO: Graph information: Is connected: true or false
+    //TODO: Graph information: List disconnected: Lists vertices that aren't connected
+
+private:
+    //TODO: Helper methods or above methods that need to access private data
+
+};
 
 #endif //GRAPH_H
